480354cd7e90d66c431ba136a19a484d7f9a3436
root <mengcz13@163.com>
2015-10-02 14:49:21 +0800

    first working

--- logicformula/urls.py
+++ logicformula/urls.py
@@ -21,4 +21,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^search/', 'logicformula.logicformula.getres'),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
